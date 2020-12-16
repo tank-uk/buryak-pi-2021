6c8aff7c3d069cef237915b5bd3ed4671288d400
Andy Karpov <andy.karpov@gmail.com>
2020-12-17 00:57:56 +0200

    build script

---
+++ firmware/src/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+export PATH=/opt/altera/quartus/bin:.platformio/penv/bin:$PATH
+
+echo "Building AVR sources"
+
+cd avr_kbd
+
+pio run -t clean
+pio run
+
+cp .pio/build/ATmega8/firmware.hex ../../releases/avr_kbd.hex
+
+pio run -t clean
+
+echo "Done"
+
+cd ..
+
+echo "Building FPGA sources"
+
+cd fpga/syn
+
+make clean
+make all
+make jic
+
+cp firmware_top.jic ../../../releases/firmware.jic
+
+make clean
+
+echo "Done"
+
+cd ../../
+
