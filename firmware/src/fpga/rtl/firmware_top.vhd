910bbfef824268e88b8a3d67056a6dbe815ced5c
Andy Karpov <andy.karpov@gmail.com>
2020-12-24 00:55:06 +0200

    Added turbosund, soundrive, saa

--- firmware/src/fpga/rtl/firmware_top.vhd
+++ firmware/src/fpga/rtl/firmware_top.vhd
@@ -85,6 +85,7 @@ architecture rtl of firmware_top is
 	signal clk_28 		: std_logic := '0';
 	signal clk_14 		: std_logic := '0';
 	signal clk_7 		: std_logic := '0';
+	signal clk_8 		: std_logic := '0';
 	signal clkcpu 		: std_logic := '1';
 	
 	signal ena_div2	: std_logic := '0';
@@ -221,154 +222,54 @@ architecture rtl of firmware_top is
 	signal host_flash_er_n : std_logic := '1';
 	
 	signal ext_rombank : std_logic_vector(2 downto 0) := "000";
+	
+	signal audio_l		: std_logic_vector(15 downto 0);
+	signal audio_r		: std_logic_vector(15 downto 0);
+
+	-- Soundrive
+	signal covox_a		: std_logic_vector(7 downto 0);
+	signal covox_b		: std_logic_vector(7 downto 0);
+	signal covox_c		: std_logic_vector(7 downto 0);
+	signal covox_d		: std_logic_vector(7 downto 0);
+
+	-- TurboSound
+	signal ssg_sel		: std_logic;
+	signal ssg0_do_bus	: std_logic_vector(7 downto 0);
+	signal ssg0_a		: std_logic_vector(7 downto 0);
+	signal ssg0_b		: std_logic_vector(7 downto 0);
+	signal ssg0_c		: std_logic_vector(7 downto 0);
+	signal ssg1_do_bus	: std_logic_vector(7 downto 0);
+	signal ssg1_a		: std_logic_vector(7 downto 0);
+	signal ssg1_b		: std_logic_vector(7 downto 0);
+	signal ssg1_c		: std_logic_vector(7 downto 0);
+
+	-- SAA1099
+	signal saa_wr_n		: std_logic;
+	signal saa_out_l	: std_logic_vector(7 downto 0);
+	signal saa_out_r	: std_logic_vector(7 downto 0);
+	
+	component saa1099
+	port (
+		clk_sys		: in std_logic;
+		ce		: in std_logic;		--8 MHz
+		rst_n		: in std_logic;
+		cs_n		: in std_logic;
+		a0		: in std_logic;		--0=data, 1=address
+		wr_n		: in std_logic;
+		din		: in std_logic_vector(7 downto 0);
+		out_l		: out std_logic_vector(7 downto 0);
+		out_r		: out std_logic_vector(7 downto 0));
+	end component;
 
 begin
-
-	divmmc_rom <= '1' when (divmmc_disable_zxrom = '1' and divmmc_eeprom_cs_n = '0') else '0';
-	divmmc_ram <= '1' when (divmmc_disable_zxrom = '1' and divmmc_sram_cs_n = '0') else '0';
-	
-	BEEPER_L <= sound_out; -- TODO: covox, soundrive, turbosound, etc...
-	BEEPER_R <= sound_out;
-
-	ay_port <= '1' when A(7 downto 0) = x"FD" and A(15)='1' and fd_port = '1' else '0';
-	AY_BC1 <= '1' when ay_port = '1' and A(14) = '1' and N_IORQ = '0' and (N_WR='0' or N_RD='0') else '0';
-	AY_BDIR <= '1' when ay_port = '1' and N_IORQ = '0' and N_WR = '0' else '0';	
-	
-	N_NMI <= '0' when nmi = '0' else '1';
-	areset <= not locked;
-	N_RESET <= '0' when areset = '1' or reset = '0' or loader_reset = '1' or loader_act = '1' else 'Z';
-
-	--N_NMI <= '0' when nmi = '0' else '1';
-	--N_RESET <= '0' when reset = '0' else 'Z';
-	
-	 -- #FD port correction
-	 fd_sel <= '0' when D(7 downto 4) = "1101" and D(2 downto 0) = "011" else '1'; -- IN, OUT Z80 Command Latch
-
-	 process(fd_sel, N_M1, N_RESET)
-	 begin
-			if N_RESET='0' then
-				  fd_port <= '1';
-			elsif rising_edge(N_M1) then 
-				  fd_port <= fd_sel;
-			end if;
-	 end process;
-	 
-	 -- dividers
-	process (clk_28)
-	begin
-		if clk_28'event and clk_28 = '0' then
-			ena_cnt <= ena_cnt + 1;
-		end if;
-	end process;
-
-	ena_div2 <= ena_cnt(0);
-	ena_div4 <= ena_cnt(1) and ena_cnt(0);
-	ena_div8 <= ena_cnt(2) and ena_cnt(1) and ena_cnt(0);
-	ena_div16 <= ena_cnt(3) and ena_cnt(2) and ena_cnt(1) and ena_cnt(0);
-	ena_div32 <= ena_cnt(5) and ena_cnt(4) and ena_cnt(3) and ena_cnt(2) and ena_cnt(1) and ena_cnt(0);
-
-	-- CPU clock 
-	clkcpu <= clk_28 and ena_div4 when turbo = '1' else clk_28 and ena_div8;
-	CPU_CLK <= clkcpu;
-	
-	port_write <= '1' when N_IORQ = '0' and N_WR = '0' and N_M1 = '1' else '0';
-	port_read <= '1' when N_IORQ = '0' and N_RD = '0' and N_M1 = '1' else '0';
-	
-	-- read ports by CPU
-	D(7 downto 0) <= 
-		ram_do when ram_oe_n = '0' else -- #memory
-		port_7ffd when port_read = '1' and A = X"7FFD" else  -- #7FFD - system port 
-		"00000" & ram_ext when port_read = '1' and A = X"DFFD" else  -- #DFFD - system port 
-		'1' & TAPE_IN & '1' & kb(4 downto 0) when port_read = '1' and A(0) = '0' else -- #FE - keyboard 
-		"000" & joy when port_read = '1' and A(7 downto 0) = X"1F" else -- #1F - kempston joy
-		divmmc_do when divmmc_wr = '1' else 									 -- divMMC
-		zc_do_bus when port_read = '1' and A(7 downto 6) = "01" and A(4 downto 0) = "10111" and enable_zcontroller else -- Z-controller
-		attr_r when port_read = '1' and A(7 downto 0) = x"FF" else -- #FF - attributes
-		"ZZZZZZZZ";
-
-	divmmc_enable <= '1' when enable_divmmc and SD_NDET = '0' else '0';
-	
-	-- z-controller 
-	zc_wr <= '1' when (enable_zcontroller and N_IORQ = '0' and N_WR = '0' and A(7 downto 6) = "01" and A(4 downto 0) = "10111") else '0';
-	zc_rd <= '1' when (enable_zcontroller and N_IORQ = '0' and N_RD = '0' and A(7 downto 6) = "01" and A(4 downto 0) = "10111") else '0';
-	
-	-- clocks
-	process (CLK_28)
-	begin 
-		if (CLK_28'event and CLK_28 = '1') then 
-			clk_14 <= not(clk_14);
-		end if;
-	end process;
-	
-	process (clk_14)
-	begin 
-		if (clk_14'event and clk_14 = '1') then 
-			clk_7 <= not(clk_7);
-		end if;
-	end process;
-	
-	cs_dffd <= '1' when N_IORQ = '0' and N_M1 = '1' and A = X"DFFD" and fd_port = '1' else '0';
-	cs_7ffd <= '1' when N_IORQ = '0' and N_M1 = '1' and A = X"7FFD" else '0';
-	cs_xxfd <= '1' when N_IORQ = '0' and N_M1 = '1' and A(15) = '0' and A(1) = '0' and fd_port = '0' else '0';
-	
-	ram_ext <= '0' & port_7ffd(6) & port_7ffd(7) when ram_ext_std = 0 else 
-				  port_7ffd(5) & port_7ffd(6) & port_7ffd(7) when ram_ext_std = 1 else
-				  port_dffd(2 downto 0) when ram_ext_std = 2 else 
-				  "000";	
-				  
-	-- ports, write by CPU
-	process( clk_28, clk_14, clk_7, N_RESET, A, D, port_write, port_7ffd, N_M1, N_MREQ )
-	begin
-		if N_RESET = '0' then
-			port_7ffd <= "00000000";
-			--sound_out <= '0';
-			if (enable_zcontroller) then 
-				trdos <= '1'; -- 1 - boot into service rom, 0 - boot into 128 menu
-			else 
-				trdos <= '0';
-			end if;
-		elsif clk_28'event and clk_28 = '1' then 
---			if clk_14 = '1' and (TURBO = '1' or clk_7 = '1') then
-				if port_write = '1' then
-
-					 -- port #7FFD  
-					if cs_7ffd = '1' and port_7ffd(5) = '0' then 
-						port_7ffd <= D;
-					elsif cs_xxfd = '1' and port_7ffd(5) = '0' then 
-						port_7ffd <= D;
-					end if;
-					 
-					-- port #DFFD (profi ram ext)
-					if cs_dffd = '1' and port_7ffd(5) = '0' and fd_port='1' then
-							port_dffd <= D(2 downto 0);
-					end if;
-					
-					-- port #FE
-					if A(0) = '0' then
-						border_attr <= D(2 downto 0); -- border attr
-						TAPE_OUT <= D(3);
-						sound_out <= D(4); -- BEEPER
-					end if;				
-					
-				end if;
-				
-				-- trdos flag
-				if enable_zcontroller and N_M1 = '0' and N_MREQ = '0' and A(15 downto 8) = X"3D" and port_7ffd(4) = '1' then 
-					trdos <= '1';
-				elsif enable_zcontroller and N_M1 = '0' and N_MREQ = '0' and A(15 downto 14) /= "00" then 
-					trdos <= '0'; 
-				end if;
-				
---			end if;
-		end if;
-	end process;	
 
 	-- PLL1
 	U1: entity work.altpll0
 	port map (
 		inclk0			=> CLK_50MHZ,
 		locked			=> locked,
-		c0 				=> clk_28
+		c0 				=> clk_28,
+		c1 				=> clk_8
 	);
 	
 	-- memory arbiter
@@ -617,6 +518,81 @@ port map(
 	LOADER_RESET 	=> loader_reset
 );	
 
+-- Soundrive
+U11: entity work.soundrive
+port map (
+	I_RESET		=> not N_RESET,
+	I_CLK		=> clk_28,
+	I_CS		=> '1',
+	I_WR_N		=> N_WR,
+	I_ADDR		=> A(7 downto 0),
+	I_DATA		=> D,
+	I_IORQ_N	=> N_IORQ,
+	I_DOS		=> trdos,
+	O_COVOX_A	=> covox_a,
+	O_COVOX_B	=> covox_b,
+	O_COVOX_C	=> covox_c,
+	O_COVOX_D	=> covox_d);
+	
+-- TurboSound
+U12: entity work.turbosound
+port map (
+	I_CLK		=> clk_28,
+	I_ENA		=> ena_div16,
+	I_ADDR		=> A,
+	I_DATA		=> D,
+	I_WR_N		=> N_WR,
+	I_IORQ_N	=> N_IORQ,
+	I_M1_N		=> N_M1,
+	I_RESET_N	=> N_RESET,
+	I_MODE   => '1', -- AY
+	O_SEL		=> ssg_sel,
+	-- ssg0
+	O_SSG0_DA	=> ssg0_do_bus,
+	O_SSG0_AUDIO_A	=> ssg0_a,
+	O_SSG0_AUDIO_B	=> ssg0_b,
+	O_SSG0_AUDIO_C	=> ssg0_c,
+	-- ssg1
+	O_SSG1_DA	=> ssg1_do_bus,
+	O_SSG1_AUDIO_A	=> ssg1_a,
+	O_SSG1_AUDIO_B	=> ssg1_b,
+	O_SSG1_AUDIO_C	=> ssg1_c);
+
+-- saa
+U13: saa1099
+port map(
+	clk_sys		=> clk_8,
+	ce		=> '1',			-- 8 MHz
+	rst_n		=> N_RESET,
+	cs_n		=> '0',
+	a0		=> A(8),		-- 0=data, 1=address
+	wr_n		=> saa_wr_n,
+	din		=> D,
+	out_l		=> saa_out_l,
+	out_r		=> saa_out_r);
+
+	
+-- Delta-Sigma
+U14: entity work.dac
+generic map (
+	msbi_g		=> 15)
+port map (
+	I_CLK		=> clk_28,
+	I_RESET		=> not N_RESET,
+	I_DATA		=> audio_l,
+	O_DAC		=> BEEPER_L);
+
+U15: entity work.dac
+generic map (
+	msbi_g		=> 15)
+port map (
+	I_CLK		=> clk_28,
+	I_RESET		=> not N_RESET,
+	I_DATA		=> audio_r,
+	O_DAC		=> BEEPER_R);
+	
+-- --------------------------------------------------------------------------------------------------------------------------------
+
 SD_NCS	<= '1' when loader_act = '1' else divmmc_sd_cs_n 	when enable_divmmc else zc_sd_cs_n 		when enable_zcontroller else '1';
 sd_clk 	<= '1' when loader_act = '1' else divmmc_sd_clk 	when enable_divmmc else zc_sd_clk	 	when enable_zcontroller else '1';
 sd_si 	<= '1' when loader_act = '1' else divmmc_sd_di 		when enable_divmmc else zc_sd_di 		when enable_zcontroller else '1';
@@ -633,5 +609,162 @@ flash_di_bus <= "00000000";
 flash_wr_n <= '1'; -- write
 flash_rd_n <= loader_flash_rd_n when loader_act = '1' else '1';
 flash_er_n <= '1'; -- erase
+
+divmmc_rom <= '1' when (divmmc_disable_zxrom = '1' and divmmc_eeprom_cs_n = '0') else '0';
+divmmc_ram <= '1' when (divmmc_disable_zxrom = '1' and divmmc_sram_cs_n = '0') else '0';
+
+ay_port <= '1' when A(7 downto 0) = x"FD" and A(15)='1' and fd_port = '1' else '0';
+AY_BC1 <= '1' when ay_port = '1' and A(14) = '1' and N_IORQ = '0' and (N_WR='0' or N_RD='0') else '0';
+AY_BDIR <= '1' when ay_port = '1' and N_IORQ = '0' and N_WR = '0' else '0';	
+
+N_NMI <= '0' when nmi = '0' else '1';
+areset <= not locked;
+N_RESET <= '0' when areset = '1' or reset = '0' or loader_reset = '1' or loader_act = '1' else 'Z';
+
+--N_NMI <= '0' when nmi = '0' else '1';
+--N_RESET <= '0' when reset = '0' else 'Z';
+
+ -- #FD port correction
+ fd_sel <= '0' when D(7 downto 4) = "1101" and D(2 downto 0) = "011" else '1'; -- IN, OUT Z80 Command Latch
+
+ process(fd_sel, N_M1, N_RESET)
+ begin
+		if N_RESET='0' then
+			  fd_port <= '1';
+		elsif rising_edge(N_M1) then 
+			  fd_port <= fd_sel;
+		end if;
+ end process;
+ 
+ -- dividers
+process (clk_28)
+begin
+	if clk_28'event and clk_28 = '0' then
+		ena_cnt <= ena_cnt + 1;
+	end if;
+end process;
+
+ena_div2 <= ena_cnt(0);
+ena_div4 <= ena_cnt(1) and ena_cnt(0);
+ena_div8 <= ena_cnt(2) and ena_cnt(1) and ena_cnt(0);
+ena_div16 <= ena_cnt(3) and ena_cnt(2) and ena_cnt(1) and ena_cnt(0);
+ena_div32 <= ena_cnt(5) and ena_cnt(4) and ena_cnt(3) and ena_cnt(2) and ena_cnt(1) and ena_cnt(0);
+
+-- CPU clock 
+clkcpu <= clk_28 and ena_div4 when turbo = '1' else clk_28 and ena_div8;
+CPU_CLK <= clkcpu;
+
+port_write <= '1' when N_IORQ = '0' and N_WR = '0' and N_M1 = '1' else '0';
+port_read <= '1' when N_IORQ = '0' and N_RD = '0' and N_M1 = '1' else '0';
+
+-- read ports by CPU
+D(7 downto 0) <= 
+	ram_do when ram_oe_n = '0' else -- #memory
+	port_7ffd when port_read = '1' and A = X"7FFD" else  -- #7FFD - system port 
+	"00000" & ram_ext when port_read = '1' and A = X"DFFD" else  -- #DFFD - system port 
+	'1' & TAPE_IN & '1' & kb(4 downto 0) when port_read = '1' and A(0) = '0' else -- #FE - keyboard 
+	"000" & joy when port_read = '1' and A(7 downto 0) = X"1F" else -- #1F - kempston joy
+	divmmc_do when divmmc_wr = '1' else 									 -- divMMC
+	ssg0_do_bus when port_read = '1' and A = X"FFFD" and ssg_sel = '0' else -- Turbosound 
+	ssg1_do_bus when port_read = '1' and A = X"FFFD" and ssg_sel = '1' else
+	zc_do_bus when port_read = '1' and A(7 downto 6) = "01" and A(4 downto 0) = "10111" and enable_zcontroller else -- Z-controller
+	attr_r when port_read = '1' and A(7 downto 0) = x"FF" else -- #FF - attributes
+	"ZZZZZZZZ";
+
+divmmc_enable <= '1' when enable_divmmc and SD_NDET = '0' else '0';
+
+-- z-controller 
+zc_wr <= '1' when (enable_zcontroller and N_IORQ = '0' and N_WR = '0' and A(7 downto 6) = "01" and A(4 downto 0) = "10111") else '0';
+zc_rd <= '1' when (enable_zcontroller and N_IORQ = '0' and N_RD = '0' and A(7 downto 6) = "01" and A(4 downto 0) = "10111") else '0';
+
+-- clocks
+process (CLK_28)
+begin 
+	if (CLK_28'event and CLK_28 = '1') then 
+		clk_14 <= not(clk_14);
+	end if;
+end process;
+
+process (clk_14)
+begin 
+	if (clk_14'event and clk_14 = '1') then 
+		clk_7 <= not(clk_7);
+	end if;
+end process;
+
+cs_dffd <= '1' when N_IORQ = '0' and N_M1 = '1' and A = X"DFFD" and fd_port = '1' else '0';
+cs_7ffd <= '1' when N_IORQ = '0' and N_M1 = '1' and A = X"7FFD" else '0';
+cs_xxfd <= '1' when N_IORQ = '0' and N_M1 = '1' and A(15) = '0' and A(1) = '0' and fd_port = '0' else '0';
+
+ram_ext <= '0' & port_7ffd(6) & port_7ffd(7) when ram_ext_std = 0 else 
+			  port_7ffd(5) & port_7ffd(6) & port_7ffd(7) when ram_ext_std = 1 else
+			  port_dffd(2 downto 0) when ram_ext_std = 2 else 
+			  "000";	
+			  
+-- ports, write by CPU
+process( clk_28, clk_14, clk_7, N_RESET, A, D, port_write, port_7ffd, N_M1, N_MREQ )
+begin
+	if N_RESET = '0' then
+		port_7ffd <= "00000000";
+		--sound_out <= '0';
+		if (enable_zcontroller) then 
+			trdos <= '1'; -- 1 - boot into service rom, 0 - boot into 128 menu
+		else 
+			trdos <= '0';
+		end if;
+	elsif clk_28'event and clk_28 = '1' then 
+--			if clk_14 = '1' and (TURBO = '1' or clk_7 = '1') then
+			if port_write = '1' then
+
+				 -- port #7FFD  
+				if cs_7ffd = '1' and port_7ffd(5) = '0' then 
+					port_7ffd <= D;
+				elsif cs_xxfd = '1' and port_7ffd(5) = '0' then 
+					port_7ffd <= D;
+				end if;
+				 
+				-- port #DFFD (profi ram ext)
+				if cs_dffd = '1' and port_7ffd(5) = '0' and fd_port='1' then
+						port_dffd <= D(2 downto 0);
+				end if;
+				
+				-- port #FE
+				if A(0) = '0' then
+					border_attr <= D(2 downto 0); -- border attr
+					TAPE_OUT <= D(3);
+					sound_out <= D(4); -- BEEPER
+				end if;				
+				
+			end if;
+			
+			-- trdos flag
+			if enable_zcontroller and N_M1 = '0' and N_MREQ = '0' and A(15 downto 8) = X"3D" and port_7ffd(4) = '1' then 
+				trdos <= '1';
+			elsif enable_zcontroller and N_M1 = '0' and N_MREQ = '0' and A(15 downto 14) /= "00" then 
+				trdos <= '0'; 
+			end if;
+			
+--			end if;
+	end if;
+end process;	
 	
+	
+saa_wr_n <= '0' when (N_IORQ = '0' and N_WR = '0' and A(7 downto 0) = "11111111" and trdos = '0') else '1';
+
+audio_l	<= ("000" & sound_out & "000000000000") + 
+				("000" & ssg0_a & "00000") + 
+				("000" & ssg0_b & "00000") + 
+				("000" & ssg1_a & "00000") + 
+				("000" & ssg1_b & "00000") + 
+				("000" & covox_a & "00000") + 
+				("000" & covox_b & "00000") + 
+				("000" & saa_out_l & "00000");
+audio_r	<= ("000" & sound_out & "000000000000") + 
+				("000" & ssg0_c & "00000") + 
+				("000" & ssg0_b & "00000") + 
+				("000" & ssg1_c & "00000") + 
+				("000" & ssg1_b & "00000") + 
+				("000" & covox_c & "00000") + 
+				("000" & covox_d & "00000") + 
+				("000" & saa_out_r & "00000");
 end;
